e9d40e8e405d948f3e49057398ca9040e9c9431a
mrevanth6 <mekala.revanth990@gmail.com>
2026-06-27 21:33:20 +0530

    Added OTP based signup and verification using SMTP

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,14 +1,17 @@
+require('dotenv').config();
 const nodeMailer = require('nodemailer');
 const transporter = nodeMailer.createTransport({
-    service: 'gmail',
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: false,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
     }
 })
 async function sendEmail(to, subject, text) {
     const mailOptions = {
-        from: process.env.EMAIL_USER,
+        from: `"AI Resume Analyser"<${process.env.EMAIL_FROM}>`,
         to: to,
         subject: subject,
         text: text
@@ -16,7 +19,7 @@ async function sendEmail(to, subject, text) {
     try {
         await transporter.sendMail(mailOptions);
     } catch (error) {
-        console.error(`Error sending email to ${to}:`, error);
+        console.error(`Error sending email to ${to}: `, error);
         throw error;
     }
 }
