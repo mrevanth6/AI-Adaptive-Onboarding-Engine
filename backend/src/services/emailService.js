de65187cc5003c46295c99561070ddeadf23d66c
mrevanth6 <mekala.revanth990@gmail.com>
2026-06-27 21:54:01 +0530

    Switched from BREVO SMTP to BREVO API KEY

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,23 +1,32 @@
 require('dotenv').config();
-const nodeMailer = require('nodemailer');
-const transporter = nodeMailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: false,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-    }
-})
+const { BrevoClient } = require('@getbrevo/brevo');
+const brevo = new BrevoClient({
+    apiKey: process.env.BREVO_API_KEY
+});
+// const transporter = nodeMailer.createTransport({
+//     host: process.env.SMTP_HOST,
+//     port: Number(process.env.SMTP_PORT),
+//     secure: false,
+//     auth: {
+//         user: process.env.SMTP_USER,
+//         pass: process.env.SMTP_PASS
+//     }
+// })
 async function sendEmail(to, subject, text) {
+    // const mailOptions = {
+    //     from: `"AI Resume Analyser"<${process.env.EMAIL_FROM}>`,
+    //     to: to,
+    //     subject: subject,
+    //     text: text
+    // };
     const mailOptions = {
-        from: `"AI Resume Analyser"<${process.env.EMAIL_FROM}>`,
-        to: to,
+        sender: { email: process.env.EMAIL_FROM, name: "AI Resume Analyser" },
+        to: [{ email: to }],
         subject: subject,
-        text: text
-    };
+        textContent: text
+    }
     try {
-        await transporter.sendMail(mailOptions);
+        await brevo.transactionalEmails.sendTransacEmail(mailOptions);
     } catch (error) {
         console.error(`Error sending email to ${to}: `, error);
         throw error;
