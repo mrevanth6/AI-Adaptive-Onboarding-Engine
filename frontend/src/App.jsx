ab3a0da8442b079e6a7efa21a852c98a0af377e0
mrevanth6 <mekala.revanth990@gmail.com>
2026-07-05 21:35:31 +0530

    Add Vercel Analytics integration and update dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,51 @@
 import ResumeAnalyzer from "./pages/ResumeAnalyzer";
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LearningRoadMap from "./pages/LearningRoadMap.jsx";
 import ResumeAnalyser from "./pages/ResumeAnalyzer.jsx";
 import LoginInPage from "./pages/LoginInPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import Layout from "./components/Layout.jsx";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import SavedRoadMaps from "./components/SavedRoadMaps.jsx";
 function App() {
   return (
-    <Router>
-      <div >
-        <ToastContainer
-          position="top-right"
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop={true}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss={false}
-        />
-        <Routes>
-
-          <Route path="/" element={<LoginInPage />} />
-          <Route path="/login" element={<LoginInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route element={
-            <ProtectedRoute>
-              <Layout />
-            </ProtectedRoute>}>
-            <Route path="/analyzer" element={<ResumeAnalyser />} />
-            <Route path="/roadmap" element={<LearningRoadMap />} />
-            <Route path="/saved-roadmaps" element={<SavedRoadMaps />} />
-          </Route>
-        </Routes>
-      </div>
-    </Router>
-  )
+    <>
+      <Router>
+        <div>
+          <ToastContainer
+            position="top-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={true}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss={false}
+          />
+          <Routes>
+            <Route path="/" element={<LoginInPage />} />
+            <Route path="/login" element={<LoginInPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route
+              element={
+                <ProtectedRoute>
+                  <Layout />
+                </ProtectedRoute>
+              }
+            >
+              <Route path="/analyzer" element={<ResumeAnalyser />} />
+              <Route path="/roadmap" element={<LearningRoadMap />} />
+              <Route path="/saved-roadmaps" element={<SavedRoadMaps />} />
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+      <Analytics />
+    </>
+  );
 }
 
-export default App
+export default App;
