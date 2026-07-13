d788573229206afce9f2d0dcd618602787316817
mrevanth6 <mekala.revanth990@gmail.com>
2026-07-13 22:37:09 +0530

    uptime robot is added to keep backend server alive

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,33 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 const app = express();
-const uploadRoute = require('./routes/uploadRoute');
-const authRoutes = require('./routes/authRoutes');
-const otpRoutes = require('./routes/otpRoutes');
-const connectTODB = require('./database/data_base');
+const uploadRoute = require("./routes/uploadRoute");
+const authRoutes = require("./routes/authRoutes");
+const otpRoutes = require("./routes/otpRoutes");
+const connectTODB = require("./database/data_base");
 
 connectTODB();
 app.use(cors());
 app.use(express.json());
-app.use('/api', uploadRoute);
-app.use('/api/auth', authRoutes);
-app.use('/api/auth/otp', otpRoutes);
-app.use((err, req, res, next) => {
-    // Determine correct status code
-    const status = err.status || err.statusCode || 500;
-    const isClientError = status >= 400 && status < 500;
-    // Send safe response
-    res.status(status).json({
-        success: false,
-        message: isClientError ? err.message : 'Internal server error'
-    });
+app.use("/api", uploadRoute);
+app.use("/api/auth", authRoutes);
+app.use("/api/auth/otp", otpRoutes);
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
 });
-
+app.use((err, req, res, next) => {
+  // Determine correct status code
+  const status = err.status || err.statusCode || 500;
+  const isClientError = status >= 400 && status < 500;
+  // Send safe response
+  res.status(status).json({
+    success: false,
+    message: isClientError ? err.message : "Internal server error",
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
